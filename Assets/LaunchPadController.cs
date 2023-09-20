239cdab44cbde2774db6622d0cfe2a9291b07744
Seamus-C <112724868+Seamus-C@users.noreply.github.com>
2023-09-20 15:11:34 -0700

    work on animaion triger

--- Assets/LaunchPadController.cs
+++ Assets/LaunchPadController.cs
@@ -2,23 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaunchPadController : MonoBehaviour {
-
+public class LaunchPadController : MonoBehaviour
+{
+    
     [SerializeField] private Animator myLanchPad = null;
 
-    [SerializeField] private bool launchTrigger = false;
-    [SerializeField] private bool unlaunchTrigger = false;
+    public Animation animation; 
+    /*
+    public KeyCode jumpKey = KeyCode.Space;
 
+    public bool readyToLaunch;
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
-
-            if (launchTrigger) {
-
-                myLanchPad.Play("Launch", 0, 0.0f);
-                gameObject.SetActive(false);
-            }
+    private void Update()
+    {
+        readyToLaunch = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsLaunch);
+    }
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PlayerMovement.Launch)
+        {
+            myLanchPad.
         }
 
     }
+
+     if (collision.gameObject.CompareTag("LaunchPad"))
+        {
+            Launch();
 }
+}    
