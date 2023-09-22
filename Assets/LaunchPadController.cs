cde6fd611afc075de0103543cbe41b6b28360137
Seamus-C <112724868+Seamus-C@users.noreply.github.com>
2023-09-22 11:55:21 -0700

    level 1

--- Assets/LaunchPadController.cs
+++ Assets/LaunchPadController.cs
@@ -9,12 +9,22 @@ public class LaunchPadController : MonoBehaviour
 
     [SerializeField] private string animationName = "Launch";
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Is tigering!");
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.Play(animationName, 0, 0f);
+        }
+    }
+}
 
-    private void OnTriggerEnter(Collider other) {
+   /*private void OnTriggerEnter(Collider other) {
         Debug.Log("Is tigering!");
         if (other.CompareTag("Player")) {
             animator.Play(animationName, 0, 0f);
         }
     }
-
-}    
+   
+}
+*/
